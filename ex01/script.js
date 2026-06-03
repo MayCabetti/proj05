5634f995128de63605f1f2d83ebb3e2f50e6b760
MayCabetti <mayaracabetti@gmail.com>
2026-06-03 08:16:14 -0300

    Sistema de armazenamento de nomes e exposição 'invertida'

---
+++ ex01/script.js
@@ -0,0 +1,15 @@
+let nomes = [];
+
+for (let i = 0; i < 7; i++) {
+    nomes[i] = prompt("Digite um nome:");
+}
+
+    alert(
+    nomes[6] + "\n" +
+    nomes[5] + "\n" +
+    nomes[4] + "\n" +
+    nomes[3] + "\n" +
+    nomes[2] + "\n" +
+    nomes[1] + "\n" +
+    nomes[0]
+);
